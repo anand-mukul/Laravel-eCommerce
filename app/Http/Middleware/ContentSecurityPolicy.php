ede61a7369cc0c465f891b17d3182852969fd17e
Mukul Anand <mukulanand.biz@gmail.com>
2025-07-20 15:44:33 +0530

    Fix CSP for inline styles on Render

--- app/Http/Middleware/ContentSecurityPolicy.php
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -3,30 +3,16 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class ContentSecurityPolicy
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): \Symfony\Component\HttpFoundation\Response  $next
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function handle(Request $request, Closure $next): Response
+    public function handle($request, Closure $next)
     {
         $response = $next($request);
 
-        $response->headers->set('Content-Security-Policy', "
-            default-src 'self';
-            style-src 'self' 'unsafe-inline';
-            script-src 'self' 'unsafe-inline' 'unsafe-eval';
-            img-src 'self' data:;
-            font-src 'self';
-            connect-src 'self';
-        ");
+        $response->headers->set('Content-Security-Policy',
+            "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline';"
+        );
 
         return $response;
     }
